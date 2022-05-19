59b97172ec5f683de62cadd91ea166ed7751eecf
wxh <xiaohanwu12@gmail.com>
2022-05-19 04:02:59 +0000

    Add release.js

--- scripts/release.js
+++ scripts/release.js
@@ -1,47 +1,68 @@
-import { exec } from "node:child_process";
-import { fileURLToPath } from "node:url";
-import path from "node:path";
+import { exec } from 'node:child_process';
+import { readJson, writeJson, processFile } from './utils.js';
 
-async function run() {
-    const { runGit } = await import("./utils.js");
-    const importDefault = async (module) => (await import(module)).default;
-    const minimist = await importDefault("minimist");
-    const params = minimist(process.argv.slice(2), {
-        string: ["version"],
-        boolean: ["dry"],
-        alias: { v: "version" },
-    });
-    // let fs = require('fs');
-    // const { stdout: previousVersion } = await runGit([
-    //     "describe",
-    //     "--tags",
-    //     "--abbrev=0",
-    //   ]);
-    // console.log(previousVersion);
-    // fs.readFile(someFile, 'utf8', function (err,data) {
-    //     if (err) {
-    //       return console.log(err);
-    //     }
-    //     var result = data.replace(/string to be replaced/g, 'replacement');
-      
-    //     fs.writeFile(someFile, result, 'utf8', function (err) {
-    //        if (err) return console.log(err);
-    //     });
-    //   });
+async function bump({ version }) {
+  const pkg = await readJson('package.json');
+  pkg.version = version;
+  await writeJson('package.json', pkg);
+
+  // Update github issue templates
+  processFile('publish/update_information.json', (content) =>
+    content.replace(/(\"latest_version\")\:.*?,/g, `$1: "${version}",`)
+  );
+  processFile('src/mock/background.data.ts', (content) =>
+    content.replace(/(latest_version)\:.*?,/g, `$1: '${version}',`)
+  );
 }
 
+async function run() {
+  const { runGit } = await import('./utils.js');
+  const importDefault = async (module) => (await import(module)).default;
+  const minimist = await importDefault('minimist');
+  const semver = await importDefault('semver');
+  const params = minimist(process.argv.slice(2), {
+    string: ['version'],
+    boolean: ['dry'],
+    alias: { v: 'version' },
+  });
+  const { stdout: previousVersion } = await runGit([
+    'describe',
+    '--tags',
+    '--abbrev=0',
+  ]);
+  if (semver.parse(previousVersion) === null) {
+    throw new Error(`Unexpected previousVersion: ${previousVersion}`);
+  } else {
+    params.previousVersion = previousVersion;
+    params.previousVersionOnDefaultBranch = (
+      await readJson('package.json')
+    ).version;
+  }
+  const steps = await Promise.all([bump]);
+
+  try {
+    for (const step of steps) {
+      await step(params);
+    }
+  } catch (error) {
+    const message = error.message.trim();
+    const stack = error.stack.replace(message, '');
+    console.error(`${'error'} ${message}\n${stack}`);
+    process.exit(1);
+  }
+}
 
 exec(
-    [
-    //   "git fetch --tags", // Fetch git tags to get the previous version number (i.e. the latest tag)
-        "echo hello"
-    ].join(" && "),
-    (error) => {
-      if (error) {
-        console.error(error);
-        process.exit(1);
-      } else {
-        run();
-      }
+  [
+    'git fetch --tags', // Fetch git tags to get the previous version number (i.e. the latest tag)
+    'git tag -d latest', // Delete the "latest" tag
+  ].join(' && '),
+  (error) => {
+    if (error) {
+      console.error(error);
+      process.exit(1);
+    } else {
+      run();
     }
-  );
+  }
+);
