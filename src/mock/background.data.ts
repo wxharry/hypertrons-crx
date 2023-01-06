107ca80e665c313362f2681a1895454d98b63613
GitHub Action <action@github.com>
2023-01-06 19:38:17 +0000

    chore(release): v1.5.8

--- src/mock/background.data.ts
+++ src/mock/background.data.ts
@@ -1,14 +1,14 @@
 export const updateInformation = {
   chrome: {
-    latest_version: '1.8.2',
+    latest_version: '1.5.8',
     url: 'https://github.com/hypertrons/hypertrons-crx/releases',
   },
   edge: {
-    latest_version: '1.8.2',
+    latest_version: '1.5.8',
     url: 'https://github.com/hypertrons/hypertrons-crx/releases',
   },
   develop: {
-    latest_version: '1.8.2',
+    latest_version: '1.5.8',
     url: 'https://github.com/hypertrons/hypertrons-crx/releases',
   },
 };
